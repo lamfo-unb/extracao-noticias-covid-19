a8ab4aa9f682437d734fd4dde66ba85a2c921821
neuremberg <neur.matos@gmail.com>
2020-03-24 14:46:05 -0300

    Add exportacao de dados ao main

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ Created on Tue Mar 24 14:21:26 2020
 """
 
 from scrapers.brasil_io import BrasilIO
+import pandas as pd 
 
 brasil_io = BrasilIO()
-casos = BrasilIO.extrair_dados()
+casos = brasil_io.extrair_dados()
+
+csv_args = {'sep': ';', 'decimal': ',', 'encoding': 'cp1252', 'index': False}
+casos.to_csv('resultados/brasil-covid19-brasil-io.csv', **csv_args)
