28a5154bafd322a69c6562e41b73aad19d8af1a6
Matteo <mateus.sakata@gmail.com>
2020-03-30 13:41:21 -0500

    Fixed main.py merge conflicts

--- main.py
+++ main.py
@@ -7,15 +7,24 @@ Created on Tue Mar 24 14:21:26 2020
 import pandas as pd
 from scrapers.brasil_io import BrasilIO
 from scrapers.corona_feed import CoronaFeed
+from scrapers.scraper_artigos_abertos import ScraperArtigosAbertos 
+from scrapers.WorldometerScraper import Worldometer_Scraper
+from scrapers.CNN.CNN_Scraper_script import CNN_Scraper
+from scrapers.CNN.CNN_Scraper_script import Article
 from scrapers.scraper_artigos_abertos import ScraperArtigosAbertos
 from scrapers.data_jhon_hopkins import DataJhonHopkins
 from datetime import datetime 
 
 HOJE = datetime.today().strftime('%Y-%m-%d')
 
+
+PATH_DIR = 'dados'
+
+CNN_DIR = "resultados\\CNN Articles\\"
 # Extração brasil.io
 brasil_io = BrasilIO()
 casos = brasil_io.extrair_dados()
+
 
 # Extração dados Jhon Hopkins University
 djh = DataJhonHopkins()
@@ -32,3 +41,13 @@ artigos_abertos = ScraperArtigosAbertos(
 artigos_abertos.salvar_texto(path_dir = 'resultados/corona_feed/')
 csv_args = {'sep': ';', 'decimal': ',', 'encoding': 'cp1252', 'index': False}
 casos.to_csv(f'resultados/brasil_io/{HOJE}-brasil-covid19-brasil-io.csv', **csv_args)
+
+# Extração Worldometer
+worldometer = Worldometer_Scraper(PATH_DIR + "\\worldometer.csv")
+
+# Extração CNN
+CNN = CNN_Scraper(CNN_DIR)
+
+worldometer.run_scraper()
+CNN.run()
+
---
+++ scrapers/reset_articles.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 27 23:04:51 2020
+
+@author: piphi
+"""
+import pickle
+
+#sends an empty list to article
+
+#ONLY USE IN EMERGENCY
+article_list = []
+pickle_out = open("article.pickle","wb")
+pickle.dump(article_list, pickle_out)
+pickle_out.close()
