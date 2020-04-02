096c34f657912d0e37d25d922d882cb0e5568229
Alixandro Werneck Leite <alixandrowerneck@outlook.com>
2020-04-02 18:57:35 -0300

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -7,34 +7,36 @@ python main.py
 
 echo "Parte 2: Script para compactar arquivos"
 echo "Parte 2.1: Pasta Brasil.io"
-cd "pasta-projeto" + /resultados/brasil_io
-zip -r "brasil.io-$(date +"%Y-%m-%d").zip"*csv
+cd "pasta-projeto + /resultados/brasil_io"
+zip -r "brasil.io-$(date +"%Y-%m-%d").zip" *csv
 echo "Zipado"
 
 echo "Parte 2.2: John Hopkins"
-cd "pasta-projeto" + /resultados/brasil_io
+cd "pasta-projeto + /resultados/jhon_hopkins"
 zip -r "jhon-hopkins-$(date +"%Y-%m-%d").zip" *csv
 echo "Zipado"
 
 echo "Parte 2.3: Corona Feeds"
-cd "pasta-projeto" + /resultados/corona_feed"
+cd "pasta-projeto + /resultados/corona_feed"
 zip -r "corona-feed-$(date +"%Y-%m-%d").zip" *txt
 echo "Zipado"
 
+#Antes, você precisa instalar o dropbox no seu computador.
+
 echo "Parte 3: Envio ao Dropbox Lamfo"
 echo "Parte 3.1: Brasil IO"
-cd "pasta-projeto" + /resultados/brasil_io
-mv  "brasil.io-$(date +"%Y-%m-%d").zip" "/home/alixandro/Dropbox/resultados/brasil_io"
+cd "pasta-projeto + /resultados/brasil_io"
+mv  "brasil.io-$(date +"%Y-%m-%d").zip" "pasta remota do Dropbox + resultados/brasil_io"
 echo "Copiado"
 
 echo "Parte 3.2: John Hopkins"
-cd "pasta-projeto" + /resultados/brasil_io
-mv  "jhon-hopkins-$(date +"%Y-%m-%d").zip" "/home/alixandro/Dropbox/resultados/jhon_hopkins"
+cd "pasta-projeto + /resultados/jhon_hopkins"
+mv  "jhon-hopkins-$(date +"%Y-%m-%d").zip" "pasta remota do Dropbox + /resultados/jhon_hopkins"
 echo "Copiado"
 
 echo "Parte 3.3: Corona Feeds"
-cd "pasta-projeto" + /resultados/corona_feed"
-mv "corona-feed-$(date +"%Y-%m-%d").zip" "/home/alixandro/Dropbox/resultados/corona_feed"
+cd "pasta-projeto + /resultados/corona_feed"
+mv "corona-feed-$(date +"%Y-%m-%d").zip" "pasta remota do Dropbox + /resultados/corona_feed"
 echo "Copiado"
 
 echo "OK! Script Finalizado"
