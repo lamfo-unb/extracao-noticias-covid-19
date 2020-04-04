811da9cb5b1928c43791ac12effef5369c500e1d
neuremberg <neur.matos@gmail.com>
2020-04-04 11:02:48 -0300

    update nomes pastas resultados em run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -7,17 +7,17 @@ python main.py
 
 echo "Parte 2: Script para compactar arquivos"
 echo "Parte 2.1: Pasta Brasil.io"
-cd "pasta-projeto + /resultados/brasil_io"
+cd "pasta-projeto + /resultados/brasil-io"
 zip -r "brasil.io-$(date +"%Y-%m-%d").zip" *csv
 echo "Zipado"
 
 echo "Parte 2.2: John Hopkins"
-cd "pasta-projeto + /resultados/jhon_hopkins"
+cd "pasta-projeto + /resultados/jhon-hopkins"
 zip -r "jhon-hopkins-$(date +"%Y-%m-%d").zip" *csv
 echo "Zipado"
 
 echo "Parte 2.3: Corona Feeds"
-cd "pasta-projeto + /resultados/corona_feed"
+cd "pasta-projeto + /resultados/corona-feed"
 zip -r "corona-feed-$(date +"%Y-%m-%d").zip" *txt
 echo "Zipado"
 
@@ -25,17 +25,17 @@ echo "Zipado"
 
 echo "Parte 3: Envio ao Dropbox Lamfo"
 echo "Parte 3.1: Brasil IO"
-cd "pasta-projeto + /resultados/brasil_io"
+cd "pasta-projeto + /resultados/brasil-io"
 mv  "brasil.io-$(date +"%Y-%m-%d").zip" "pasta remota do Dropbox + resultados/brasil_io"
 echo "Copiado"
 
 echo "Parte 3.2: John Hopkins"
-cd "pasta-projeto + /resultados/jhon_hopkins"
+cd "pasta-projeto + /resultados/jhon-hopkins"
 mv  "jhon-hopkins-$(date +"%Y-%m-%d").zip" "pasta remota do Dropbox + /resultados/jhon_hopkins"
 echo "Copiado"
 
 echo "Parte 3.3: Corona Feeds"
-cd "pasta-projeto + /resultados/corona_feed"
+cd "pasta-projeto + /resultados/corona-feed"
 mv "corona-feed-$(date +"%Y-%m-%d").zip" "pasta remota do Dropbox + /resultados/corona_feed"
 echo "Copiado"
 
